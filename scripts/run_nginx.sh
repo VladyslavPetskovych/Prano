cec2d101028885762d9cf2ff82fc59b7ee12afd9
Nazarii Ryhus <superkriper45@gmail.com>
2025-01-27 20:46:08 +0200

    1

--- scripts/run_nginx.sh
+++
@@ -1,4 +0,0 @@
-echo "################################## Run nginx"
-export DOLLAR='$'
-envsubst < ./src/configs/nginx/nginx.conf.template > /etc/nginx/nginx.conf # /etc/nginx/conf.d/default.conf
-nginx -g "daemon off;"
