47b6344cba924f897e5d4b17c0c786912669a36f
Nazarii Ryhus <superkriper45@gmail.com>
2025-02-02 13:17:15 +0200

    first

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './app.js',
+    entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'dist'),
         publicPath: '/',
