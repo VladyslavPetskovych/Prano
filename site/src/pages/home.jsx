d17b2fd7c27c6be6567c162733ccd8d3ba296b02
vladyslav <peckovychv@gmail.com>
2025-02-04 10:38:18 +0200

    added block about

--- site/src/pages/home.jsx
+++ site/src/pages/home.jsx
@@ -1,11 +1,13 @@
 import EndlessPossibilities from "../components/homePage/EndlessPossibilities.jsx";
 import { TopBlock } from "../components/homePage/topBlock.jsx";
+import About from "../components/homePage/about.jsx";
 
 const Home = () => {
   return (
     <div>
       <TopBlock />
       <EndlessPossibilities />
+      <About/>
     </div>
   );
 };
