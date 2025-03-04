1ed5e643e4642cbf11f22f0c32049f744f1574fd
Nazarii Ryhus <superkriper45@gmail.com>
2025-03-04 10:39:40 +0200

    fix of search

--- server/src/utils/query.parser.js
+++ server/src/utils/query.parser.js
@@ -2,7 +2,7 @@ class QueryParser {
     static parse(query) {
         const queryStr = JSON.stringify(query)
 
-        return JSON.parse(queryStr.replace(/\b(regex|options)\b/, (match) => `$${match}`))
+        return JSON.parse(queryStr.replace(/\b(regex|options)\b/g, match => `$${match}`))
     }
 }
 
