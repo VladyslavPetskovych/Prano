5c335d11716fe788385710e51dff5ff7ea01e511
vladyslav <peckovychv@gmail.com>
2025-04-15 10:52:23 +0300

    fix

--- site/vite.config.cjs
+++ site/vite.config.cjs
@@ -1,35 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import Prerender from "vite-plugin-prerender";
-import { visualizer } from 'rollup-plugin-visualizer'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ open: false }),
-    Prerender({
-      staticDir: "dist",
-      routes: ["/"],
-    }),
   ],
 
-  server: {
-    host: true,
-  },
-  build: {
-    minify: "terser", 
-    cssCodeSplit: true, 
-    sourcemap: false, 
-    chunkSizeWarningLimit: 500, 
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
 });
