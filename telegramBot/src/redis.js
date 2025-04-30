d3a6112f7eb014d689a1aa115562164f78e5fc5f
vladyslav <peckovychv@gmail.com>
2025-04-30 10:30:20 +0300

    telegram bot auth

--- telegramBot/src/redis.js
+++ telegramBot/src/redis.js
@@ -1,7 +1,8 @@
 const redis = require("redis");
+require('dotenv').config();
 
 const client = redis.createClient({
-  url: "redis://127.0.0.1:6379", 
+  url: process.env.REDIS_URL, 
 });
 
 client.on("error", (err) => {
