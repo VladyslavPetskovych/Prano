6daec5d8a20d7495fdda6d597a19ddb680690b39
vladyslav <peckovychv@gmail.com>
2025-05-06 10:37:11 +0300

    fix

--- site/src/components/utils/towelButton.jsx
+++ site/src/components/utils/towelButton.jsx
@@ -4,7 +4,7 @@ import towel from "../../assets/utils/towel.jpg";
 function TowelButton() {
   return (
     <button
-      className="relative px-5 py-3 rounded-lg shadow-xl overflow-hidden"
+      className="relative px-10 md:px-32 py-7 rounded-lg shadow-xl overflow-hidden"
       style={{
         backgroundImage: `url(${towel})`,
         backgroundSize: "cover",
@@ -20,11 +20,11 @@ function TowelButton() {
         e.currentTarget.style.transform = "scale(1)";
       }}
     >
-      <span className="relative z-10 text-Ndark text-base font-extrabold tracking-wide uppercase drop-shadow-[2px_2px_5px_rgba(0,0,0,0.7)]">
+      <span className="relative z-10 text-white bg-Ndark rounded-xl px-4 text-base font-bold tracking-wide uppercase drop-shadow-[2px_2px_5px_rgba(0,0,0,0.7)]">
         Замовити послугу
       </span>
 
-      <div className="absolute inset-0 bg-black opacity-15 rounded-lg backdrop-blur-sm"></div>
+      <div className="absolute inset-0 bg-black opacity-25 rounded-lg backdrop-blur-sm"></div>
     </button>
   );
 }
