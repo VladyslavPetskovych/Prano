205abdc38f3f18f90a909590b2bdb2d47178bdfe
Nazarii <superkriper45@gmail.com>
2025-09-06 17:27:48 +0300

    tg bot

---
+++ telegramBot/src/handlers/createOrderHandler.js
@@ -0,0 +1,78 @@
+const bot = require("../bot");
+const redisClient = require("../redis");
+const axios = require("axios");
+
+const createOrder = async (msg, productType) => {
+    const chatId = msg.chat.id.toString();
+
+    const clothType = await redisClient.get(`${chatId}:cloth`);
+    if (!clothType) {
+        return bot.sendMessage(chatId, "⚠️ Спочатку введіть тип одягу.");
+    }
+
+    try {
+        const {data: user} = await axios.get("https://prano.group/api/telegram/user", {
+            headers: {
+                Authorization: chatId.toString(),
+            },
+        });
+
+        await axios.post("https://prano.group/api/telegram/order", {
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            clothType,
+            productType,
+        }, {
+            headers: {Authorization: chatId}
+        });
+
+        const keyboardButtons = [["💰 Послуги", "👤 Профіль"], ["✍️ Створити нове замовлення"], ["🧾 Ваша історія замовлень"], ["🚪 Вийти"]];
+
+        const keyboard = {
+            reply_markup: {
+                keyboard: keyboardButtons,
+                resize_keyboard: true,
+                one_time_keyboard: false,
+            },
+            parse_mode: "Markdown",
+            disable_web_page_preview: true,
+        };
+
+        bot.sendMessage(chatId, `✅ Замовлення створено: ${clothType} (${productType})`, keyboard);
+    } catch (e) {
+        console.error("❌ Помилка створення замовлення:", e.message);
+        bot.sendMessage(
+            chatId,
+            "⚠️ Не вдалося створити замовлення. Спробуйте пізніше."
+        );
+    }
+}
+
+bot.on("message", async (msg) => {
+    const text = msg.text;
+
+    if (text === "🧼 Хімчистка") createOrder(msg, "Хімчистка")
+    if (text === "🧶 Ремонт") createOrder(msg, "Ремонт")
+    if (text === "🚫 Скасувати") {
+        const message = "🚫 Ви скасували оформлення замовлення";
+
+        const chatId = msg.chat.id.toString();
+        const phone = await redisClient.get(chatId);
+
+        const keyboardButtons = [["💰 Послуги", "👤 Профіль"], ["✍️ Створити нове замовлення"], ["🧾 Ваша історія замовлень"]];
+        if (phone) keyboardButtons.push(["🚪 Вийти"]);
+
+        const keyboard = {
+            reply_markup: {
+                keyboard: keyboardButtons,
+                resize_keyboard: true,
+                one_time_keyboard: false,
+            },
+            parse_mode: "Markdown",
+            disable_web_page_preview: true,
+        };
+
+        bot.sendMessage(chatId, message, keyboard);
+    }
+})
